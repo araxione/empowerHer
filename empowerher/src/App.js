728624a52729f8f2b24046a11d5fabab9eb66d21
trixiec <trixiec@students.cs.ubc.ca>
2023-03-11 20:58:41 -0800

    finished landing page and nav bar

--- empowerher/src/App.js
+++ empowerher/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import NavBar from './navbar';
+import { LandingPage } from './LandingPage';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+      <NavBar />
+      <Router>
+        <div className="App">
+        <Routes>
+          <Route path='/' element={<LandingPage/>} />
+        </Routes>
+        </div>
+      </Router>
+      </>
+    );
+  }
 }
 
-export default App;
+export default App
---
+++ empowerher/src/navbar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './navbar.css';
+
+//fonts
+import './fonts/Poppins-Medium.ttf';
+import './fonts/YesevaOne-Regular.ttf';
+
+class NavBar extends Component {
+    render() { 
+        return (
+            <nav className='nav'>
+                <h1 className='nav-logo'>empowerHer</h1>
+                <ul className='nav-menu'>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/">Mentors</a>
+                    </li>
+                    <li>
+                        <a href="/">Resources</a>
+                    </li>
+                    <li>
+                        <a href="/">Contact Us</a>
+                    </li>
+                    <li>
+                        <a href="/">About</a>
+                    </li>
+                 </ul>
+            </nav>
+        )
+    }
+}
+
+export default NavBar
