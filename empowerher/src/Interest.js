641baea685dbe610e3fc6a52525a0f3e6bb67ceb
Krisha <krishaiquin@gmail.com>
2023-04-04 10:23:02 -0700

    btns are now highlighted when clicked in interest page

--- empowerher/src/Interest.js
+++ empowerher/src/Interest.js
@@ -1,5 +1,7 @@
 import './Interest.css';
 import StepProgressBar from './StepProgressBar';
+import {useState} from "react";
+import styled from "styled-components";
 
 
 //fonts
@@ -7,10 +9,65 @@ import './fonts/Poppins-Medium.ttf';
 import './fonts/Poppins-Bold.ttf';
 import './fonts/Poppins-Light.ttf';
 
+const Button = styled.button`
+  padding: 20px 10px 20px 10px;
+  margin: 0px 20px 20px 20px;
+  border-radius: 12px;
+  border-width: 1px;
+  background-color: white;
+  font-family: "Poppins-bold";
+  &:hover{
+    background-color: #FDE8EB;
+  }
+`;
+
+const ButtonToggle = styled(Button)`
+  ${({ active }) =>
+    active &&
+    `
+    background-color: #FDE8EB; 
+  `}
+`;
+
+
+
+const interests = [
+  "Data Science",
+  "Front-End",
+  "Back-End",
+  "UX Design",
+  "Web Design",
+  "Graphic Design",
+  "Product Design",
+  "Product Management",
+  "Cybersecurity",
+  "System Analysis",
+  "Cloud Computing",
+  "Support Specialist",
+  "Database Administration",
+  "Network Engineering",
+];
+
+function ButtonGroup() {
+  const [active, setActive] = useState();
+  console.log(active);
+
+  return(
+    <>
+      {interests.map((interest) =>(
+        <ButtonToggle active = {active===interest}  onClick = {() => setActive(interest)}>{interest}</ButtonToggle>
+      ) )};
+    </>
+  );
+
+  
+}
+
 
 export const Interest = () => {
+
   return (
-    <div className="Interest">
+    <>
       <div className='Progress-bar'>
         <StepProgressBar />
       </div>
@@ -20,25 +77,12 @@ export const Interest = () => {
         <p>We would love to get to know you more! Please let us know what your interets are. Select up to 3 choices.</p>
       </div>
       <div className='Button-group'>
-        <button className='Select-button'>Data Science</button>
-        <button className='Select-button'>Front-End</button>
-        <button className='Select-button'>Back-End</button>
-        <button className='Select-button'>UX Design</button>
-        <button className='Select-button'>Web Design</button>
-        <button className='Select-button'>Graphic Design</button>
-        <button className='Select-button'>Product Design</button>
-        <button className='Select-button'>Product Management</button>
-        <button className='Select-button'>Cybersecurity</button>
-        <button className='Select-button'>System Analysis</button>
-        <button className='Select-button'>Cloud Computing</button>
-        <button className='Select-button'>Support Specialist</button>
-        <button className='Select-button'>Database Administration</button>
-        <button className='Select-button'>Network Engineering</button>       
+        <ButtonGroup />
       </div>
       <div className = "Direction-group">
         <button className='Direction-button'>Continue</button>
       </div>
-    </div>
+    </>
     
     
   );
---
+++ empowerher/src/SelectBtns.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+
+
+
+
+class SelectBtns extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            selected: null
+        }
+    }
+
+    buttonSelected = selected => ev => {
+        this.setState({ selected })
+    }
+
+    
+
+    render(){
+        return(
+            <Button className={key === this.state.selected ? 'selected' : ''}>
+                     {this.props}
+            </Button>
+        );
+    }
+}
